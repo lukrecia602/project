d550a9c685b99a69edd4c3dda8b6da518c5f4ef9
Tóth Sándorné Márta <145431244+lukrecia602@users.noreply.github.com>
2024-11-09 17:15:10 +0100

    Add files via upload

--- structdump241109.sql
+++ structdump241109.sql
@@ -16,24 +16,139 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`project` /*!40100 DEFAULT CHARACTER SET
 
 USE `project`;
 
-/*Data for the table `bonuses` */
+/*Table structure for table `bonuses` */
 
-/*Data for the table `currencies` */
+DROP TABLE IF EXISTS `bonuses`;
 
-/*Data for the table `exchanges` */
+CREATE TABLE `bonuses` (
+  `id` tinyint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `level` tinyint(4) NOT NULL,
+  `discount` tinyint(4) NOT NULL DEFAULT 0,
+  `minAmount` int(11) NOT NULL,
+  `updated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `bonuses_ibfk_1` FOREIGN KEY (`id`) REFERENCES `userlevel` (`bonusId`),
+  CONSTRAINT `bonuses_ibfk_2` FOREIGN KEY (`id`) REFERENCES `transactions` (`bonusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
-/*Data for the table `firms` */
+/*Table structure for table `currencies` */
 
-/*Data for the table `persons` */
+DROP TABLE IF EXISTS `currencies`;
 
-insert  into `persons`(`id`,`userId`,`firstName`,`lastName`,`address`,`email`,`phone`,`updated`,`deleted`) values 
-(1,2,'Kis','Kata','witgw','kiskata@freemail.hu',4294967295,'2024-11-06 11:36:59',0);
+CREATE TABLE `currencies` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` enum('HUF','EUR','AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','GBP','HKD','HRK','IDR','ILS','INR','ISK','JPY','KRW','MXN','MYR','NOK','NZD','PHP','PLN','RON','RSD','RUB','SEK','SGD','THB','TRY','UAH','USD','ZAR','ATS','AUP','BEF','BGL','CSD','CSK','DDM','DEM','EEK','EGP','ESP','FIM','FRF','GHP','GRD','IEP','ITL','KPW','KWD','LBP','LTL','LUF','LVL','MNT','NLG','OAL','OBL','OFR','ORB','PKR','PTE','ROL','SDP','SIT','SKK','SUR','VND','XEU','XTR','YUD') NOT NULL,
+  `defAmount` tinyint(4) NOT NULL DEFAULT 1,
+  `lastDate` date NOT NULL DEFAULT '2024-10-21',
+  `closed` tinyint(1) NOT NULL DEFAULT 0,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `currencies_ibfk_3` FOREIGN KEY (`id`) REFERENCES `exchanges` (`firstCur`) ON UPDATE CASCADE,
+  CONSTRAINT `currencies_ibfk_4` FOREIGN KEY (`id`) REFERENCES `exchanges` (`secondCur`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
-/*Data for the table `transactions` */
+/*Table structure for table `exchanges` */
 
-/*Data for the table `user` */
+DROP TABLE IF EXISTS `exchanges`;
 
-/*Data for the table `userlevel` */
+CREATE TABLE `exchanges` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `transId` bigint(20) unsigned NOT NULL,
+  `type` enum('HUF-other','other-HUF','cross') NOT NULL DEFAULT 'HUF-other',
+  `date` date NOT NULL,
+  `out` tinyint(1) NOT NULL DEFAULT 0,
+  `newId` bigint(20) unsigned DEFAULT NULL,
+  `firstCur` int(10) unsigned NOT NULL,
+  `secondCur` int(10) unsigned NOT NULL,
+  `value` int(11) NOT NULL DEFAULT 1,
+  `timeStamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `firstCur` (`firstCur`),
+  KEY `secondCur` (`secondCur`),
+  KEY `transId` (`transId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+/*Table structure for table `firms` */
+
+DROP TABLE IF EXISTS `firms`;
+
+CREATE TABLE `firms` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` enum('bt','kft','nyrt','zrt') NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` int(20) DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+/*Table structure for table `persons` */
+
+DROP TABLE IF EXISTS `persons`;
+
+CREATE TABLE `persons` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` int(20) unsigned DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+/*Table structure for table `transactions` */
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `bonusId` tinyint(4) unsigned NOT NULL,
+  `timeStamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `bonusId` (`bonusId`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id`) REFERENCES `exchanges` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('person','firm') NOT NULL,
+  `nationality` varchar(30) NOT NULL DEFAULT '''hungarian''',
+  `regDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id`) REFERENCES `userlevel` (`userId`) ON UPDATE CASCADE,
+  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id`) REFERENCES `firms` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_ibfk_3` FOREIGN KEY (`id`) REFERENCES `persons` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_ibfk_4` FOREIGN KEY (`id`) REFERENCES `transactions` (`userId`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+/*Table structure for table `userlevel` */
+
+DROP TABLE IF EXISTS `userlevel`;
+
+CREATE TABLE `userlevel` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `bonusId` tinyint(4) unsigned NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `bonusId` (`bonusId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
